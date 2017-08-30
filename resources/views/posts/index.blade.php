0a452a70bcee7d95277538c92a7173e58a1bc44c
Kenneth Paulson <kenpaulson@att.net>
2017-08-29 21:24:45 -0400

    testing ssh on local cpu

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,9 +9,11 @@
 			<h1>All Posts</h1>			
 		</div>
 		<div class="col-md-2">
+
 			<a href="{{ route('posts.create') }}"
 				 class="btn btn-lg btn-block btn-primary btn-h1-spacing">
 				 Create New Post</a>
+				 
 		</div>
 		<div class="col-md-12">
 			<hr>
